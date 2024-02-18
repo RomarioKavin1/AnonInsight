f1673f7012c4c8797bd1fcc6f0208e95bd6e7852
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-02-18 18:59:54 +0530

    mapped

--- frontend/src/app/viewform/[id]/page.tsx
+++ frontend/src/app/viewform/[id]/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 import ConnectButton from '@/components/ConnectButton';
+import Link from 'next/link';
 import React, { useEffect } from 'react'
 
 function  Page({ params }: { params: { id: string } }) {
+  const ratings = [
+    {  rating: 3, comment: 'Great service!' },
+    {  rating: 5, comment: 'Excellent experience!' },
+    {  rating: 4, comment: 'Very satisfied!' },
+  ];
     const [formid, setFormid] = React.useState('')
-    const rating = 3;
     const [loading, setLoading] = React.useState(true)
     useEffect(() => {
         console.log("wok");
@@ -15,8 +20,9 @@ function  Page({ params }: { params: { id: string } }) {
   return (
     <div className="h-full overflow-hidden">
     <div className="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row justify-center items-center gap-4 over">
-
-
+    <div className='w-4/5 p-3 flex justify-center items-center'>
+    <h1 className='text-3xl p-2 b-2'>Form ID :<span className='text-white'>{formid}</span></h1>
+      </div>
         <div className='flex w-4/5 justify-between'>
             <div>
                 <h1 className='text-3xl p-2 b-2'>Your Ratings</h1>
@@ -24,26 +30,32 @@ function  Page({ params }: { params: { id: string } }) {
             <div>
             <ConnectButton/>
             </div>
+
+            <Link href={'/home'}><button className='bg-slate-700 text-xl p-2 rounded-md hover:bg-slate-600'>Back</button></Link>
         </div>
-                <div className='w-4/5 bg-slate-700 rounded-md p-3 flex justify-between'>
-                    <div>
-                    <div className='flex gap-2'>
-                      <h2 className='text-xl'><span className='text-white'>1</span></h2>
-                      <h2 className='text-xl'>
-                        Rating :
-                        <span className='text-white'>
-                          {[...Array(rating)].map((_, index) => (
-                            <span key={index} className='star'>&#9733;</span>
-                          ))}
-                        </span>
-                      </h2>
-                    </div>
-                    <h2 className='text-xl'>Comment : <span className='text-white'>a</span></h2>
-                    </div>
-                    <div>
-                    </div>
+        {ratings.map(({rating, comment },index) => (
+      <div key={index} className='w-4/5 bg-slate-700 rounded-md p-3 flex justify-between'>
+        <div>
+          <div className='flex gap-2'>
+            <h2 className='text-xl'>
+              <span className='text-white'>{index+1}</span>
+            </h2>
+            <h2 className='text-xl'>
+              Rating :
+              <span className='text-white'>
+                {[...Array(rating)].map((_, index) => (
+                  <span key={index} className='star'>&#9733;</span>
+                ))}
+              </span>
+            </h2>
+          </div>
+          <h2 className='text-xl'>Comment : <span className='text-white'>{comment}</span></h2>
+        </div>
+        <div>
+        </div>
+      </div>
+    ))}
                 </div>
-    </div>
     </div>
   )
 }
